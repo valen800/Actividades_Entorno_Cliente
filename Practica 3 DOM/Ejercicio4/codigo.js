915eb8d5a5ec1c4f8da5a00411bbda2c11942bac
valen800 <alurui180@ieselcaminas.org>
2020-10-15 12:18:05 +0200

    Exercise 4 completed

---
+++ Practica 3 DOM/Ejercicio4/codigo.js
@@ -0,0 +1,23 @@
+function countLinks() {
+    nLinks = 0;
+    arrayElements = document.body.childNodes;
+
+    for (let i = 0; i < arrayElements.length; i++) {
+        if (arrayElements[i].nodeName == "A") {
+            nLinks += 1;
+        }
+    }
+    alert("Número de Links: ",nLinks)
+}
+
+function countImg() {
+    nImg = 0;
+    arrayElements = document.body.childNodes;
+
+    for (let i = 0; i < arrayElements.length; i++) {
+        if (arrayElements[i].nodeName == "IMG") {
+            nImg += 1;
+        }
+    }
+    alert("Número de imágenes: ",nImg)
+}
