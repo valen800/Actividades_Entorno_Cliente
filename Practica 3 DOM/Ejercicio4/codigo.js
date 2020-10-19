c39a5010e86b343dce46837fde5b379e7d7e8fe7
valen800 <alurui180@ieselcaminas.org>
2020-10-19 08:47:36 +0200

    bug fixed

--- Practica 3 DOM/Ejercicio4/codigo.js
+++ Practica 3 DOM/Ejercicio4/codigo.js
@@ -1,23 +1,23 @@
 function countLinks() {
     var nLinks = 0;
-    arrayElements = document.body.childNodes;
+    var arrayElements = document.body.childNodes;
 
     for (let i = 0; i < arrayElements.length; i++) {
         if (arrayElements[i].nodeName == "A") {
             nLinks += 1;
         }
     }
-    alert("Número de Links: ",nLinks)
+    alert("Número de Links: " + nLinks)
 }
 
 function countImg() {
     var nImg = 0;
-    arrayElements = document.body.childNodes;
+    var arrayElements = document.body.childNodes;
 
     for (let i = 0; i < arrayElements.length; i++) {
         if (arrayElements[i].nodeName == "IMG") {
             nImg += 1;
         }
     }
-    alert("Número de imágenes: ",nImg)
+    alert("Número de imágenes: " + nImg)
 }
