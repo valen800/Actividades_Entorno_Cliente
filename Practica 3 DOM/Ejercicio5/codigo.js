3febe52f60e804eb0f16059923f5b507d6e11490
valen800 <alurui180@ieselcaminas.org>
2020-10-19 08:16:57 +0200

    final exercises

--- Practica 3 DOM/Ejercicio5/codigo.js
+++ Practica 3 DOM/Ejercicio5/codigo.js
@@ -1,6 +1,6 @@
 function createLink() {
     var p = document.createElement("p")
     p.textContent = "Google"
-    p.setAttribute("style", "background-color: red;"); 
+    p.setAttribute("style", "background-color: red;");
     document.body.appendChild(p)
 }
