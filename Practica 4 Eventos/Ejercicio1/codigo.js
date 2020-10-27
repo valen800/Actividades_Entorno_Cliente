42d36fdb8b02c95a246767e3c21b574defc98062
Valentin Ruiz Lopez <varulo.cvcv@gmail.com>
2020-10-27 21:34:32 +0100

    Exercise 1 Events

---
+++ Practica 4 Eventos/Ejercicio1/codigo.js
@@ -0,0 +1,7 @@
+function onMouseOver() {
+    document.getElementById("texto").style.fontSize = "16pt";
+}
+
+function onMouseOut() {
+    document.getElementById("texto").style.fontSize = "12pt";
+}
