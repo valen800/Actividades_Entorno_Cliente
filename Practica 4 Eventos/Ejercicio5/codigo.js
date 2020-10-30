971680f6e7adac20e9f3c4a32ad8b27f2c41471d
Valentin Ruiz Lopez <varulo.cvcv@gmail.com>
2020-10-30 13:49:07 +0100

    exercise 5 completed

--- Practica 4 Eventos/Ejercicio5/codigo.js
+++ Practica 4 Eventos/Ejercicio5/codigo.js
@@ -1,9 +1,33 @@
-window.onload = function() {
-    var inputText = document.getElementById("inputText");
-    inputText.addEventListener("keypress", checkNumbers)
-
+function checkNumber(event) {
+    if(event.charCode >= 48 && event.charCode <= 57) {
+        return true;
+    }
+    return false;
 }
 
-function showInformation(theEvent) {
-    var localEvent = window.event || theEvent;
+
+//Solucion sin KeyCode y charCode
+function Numeros(string){//Solo numeros
+    var result = '';
+    var filtro = '1234567890';//Caracteres validos
+	
+    //Recorrer el texto y verificar si el caracter se encuentra en la lista de validos 
+    for (var i=0; i<string.length; i++)
+       if (filtro.indexOf(string.charAt(i)) != -1) 
+             //Se añaden a la salida los caracteres validos
+            result += string.charAt(i);
+	
+    //Retornar valor filtrado
+    return result;
+}
+
+function NumText(string){//solo letras y numeros
+    var out = '';
+    //Se añaden las letras validas
+    var filtro = 'abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ1234567890';//Caracteres validos
+	
+    for (var i=0; i<string.length; i++)
+       if (filtro.indexOf(string.charAt(i)) != -1) 
+	     out += string.charAt(i);
+    return out;
 }
