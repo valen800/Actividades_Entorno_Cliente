6074270f921ea7d6431acba645f51bf5d82651db
Valentin Ruiz Lopez <varulo.cvcv@gmail.com>
2020-11-01 01:34:50 +0100

    codigo de prueba

--- Practica 4 Eventos Anexo/Ejercicio2/codigo.js
+++ Practica 4 Eventos Anexo/Ejercicio2/codigo.js
@@ -1,5 +1,12 @@
 var link = document.getElementById("link");
 
-link.addEventListener("click", function(event) {
+/* link.addEventListener("click", function(event) {
     event.preventDefault()
-})
+}) */
+
+window.addEventListener("beforeunload", function (e) {
+    var confirmationMessage = "Página cancelada";
+
+    e.returnValue = confirmationMessage;     // Gecko, Trident, Chrome 34+
+    return confirmationMessage;
+});
