b525f03b6c3e33d1be005fd73bf72f34edc6702f
Valentin Ruiz Lopez <alurui180@ieselcaminas.org>
2020-11-02 10:24:54 +0100

    fixed exercise 4

--- Practica 4 Eventos/Ejercicio4/codigo.js
+++ Practica 4 Eventos/Ejercicio4/codigo.js
@@ -1,7 +1,16 @@
 window.onload = function() {
     var div = document.getElementById("text");
-    div.addEventListener("mouseover", mouseOver)
-    div.addEventListener("mouseout", mouseOut)
+
+    if (div.addEventListener) {
+      div.addEventListener("mouseover", mouseOver)
+      div.addEventListener("mouseout", mouseOut)
+    } else {
+      if (div.attachEvent) {
+        div.attachEvent("onmouseover", mouseOver)
+        div.attachEvent("onmouseout", mouseOut)
+        alert("Internet Explorer")
+      }
+    }
 }
 
 function mouseOver() {
@@ -11,3 +20,23 @@ function mouseOver() {
 function mouseOut() {
     document.getElementById("text").style.fontSize = "12pt";
 }
+
+function checkNavigator() {
+    var navigatorInfo = navigator.userAgent;
+  
+    var navigators = [
+      "Chrome",
+      "Firefox",
+      "Safari",
+      "Opera",
+      "Trident", //Internet Explorer
+      "MSIE", //Internet Explorer
+      "Edge",
+    ];
+  
+    for (var i in navigators) {
+      if (navigatorInfo.indexOf(navigators[i]) != -1) {
+        return navigators[i];
+      }
+    }
+  }
