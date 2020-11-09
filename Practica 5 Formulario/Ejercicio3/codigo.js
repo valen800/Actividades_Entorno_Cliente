4ae77febffe3e6f7dc9c23c656433ce44338a260
Valentin Ruiz Lopez <varulo.cvcv@gmail.com>
2020-11-09 18:33:38 +0100

    exercise 3 completed

---
+++ Practica 5 Formulario/Ejercicio3/codigo.js
@@ -0,0 +1,24 @@
+window.onload = function() {
+    var parrafo = document.getElementsByTagName("p")[0];
+    var radioGroup = document.getElementsByName("colors");
+
+
+    for (let i = 0; i < radioGroup.length; i++) {
+        radioGroup[i].addEventListener("change", function() {
+            checkRadioGroup(radioGroup, parrafo);
+        });
+    }
+
+}
+
+function checkRadioGroup(radioGroup, parrafo) {
+    for (let i = 0; i < radioGroup.length; i++) {
+        if (radioGroup[i].checked) {
+            changeColor(radioGroup[i].value, parrafo)
+        }
+    }
+}
+
+function changeColor(color, parrafo) {
+    parrafo.style.backgroundColor = color;
+}
